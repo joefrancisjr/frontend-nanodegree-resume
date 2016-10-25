f0b2d51c2adf501d7f6be8eefd42aa83200034e1
Joe <joefrancisjr@live.com>
2016-10-25 06:52:38 -0700

    Added name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,3 +7,9 @@
 //var funThoughts = amazingThoughts.replace("amazing", "fun")
 
 //$("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%", "Joe Francis")
+
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer")
+
+$("#header").prepend(formattedName, formattedRole);

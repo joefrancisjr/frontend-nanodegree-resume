26ac4593662d0aeab1c1a9d24dbc6cc97a6b3130
Joe <joefrancisjr@live.com>
2016-11-01 06:24:14 -0700

    added bio not yet displaying

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -13,3 +13,20 @@ var formattedName = HTMLheaderName.replace("%data%", "Joe Francis")
 var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer")
 
 $("#header").prepend(formattedName, formattedRole);
+
+var bio = {
+  "name": "Joe Francis",
+  "role": "Web Developer",
+  "contacts": {
+    "mobile": "650-762-9762",
+    "email": "joefrancisjr@live.com",
+    "github": "joefrancisjr",
+    "twitter": "joefrancisjr",
+    "location": "San Francisco"
+  },
+  "welcomeMessage": "Filler filler filler text text text text text.",
+  "skills": [
+    "large apetite", "burning desire", "fun soul", "ridiculous"
+  ],
+  "bioPic": "images/me.jpg"
+};
